4c4d122f2af1504fab491bc0e868ef4b7555d4d4
Sirawit Kamchoom <sirawitkc@gmail.com>
2024-07-13 16:09:11 +0700

    Control Flow and Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, DatePipe, DecimalPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,4 +13,21 @@ export class AppComponent {
   title = 'Patiwat : https://github.com/pppatiwatt';
   imageConverURL = '/images/patiwat.jpeg'
 
+  onImageClick() {
+    alert("pppatiwatt")
+  }
+
+  major = 'Programmer';
+
+  items = [
+    { name: 'Angular', url: 'https://angular.io' },
+    { name: 'React', url: 'https://reactjs.org' },
+    { name: 'Vue', url: 'https://vuejs.org' }
+  ];
+
+  skill = "Basic"
+
+  now = new Date();
+
+  number = 5555.5555;
 }
